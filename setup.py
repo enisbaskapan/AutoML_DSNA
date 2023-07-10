6ca19164e9b7189ce29438d48dc6750b350cddc4
Enis Başkapan <77108384+enisbaskapan@users.noreply.github.com>
2023-07-10 14:25:02 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,3 @@
 from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setup(name="AutoML_DSNA",
-    author="Vestech",
-    description="Automate machine learning",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/enisbaskapan/AutoML_DSNA",
-    packages=setuptools.find_packages(),
-    python_requires=">=3.10",)
+setup()

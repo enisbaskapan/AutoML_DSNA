66d2e0f2bb7bf40134588bd13cd17b2ab5dac0a4
Enis Başkapan <77108384+enisbaskapan@users.noreply.github.com>
2023-07-10 13:46:07 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(name="AutoML_DSNA",
     description="Automate machine learning",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/enisbaskapan/AutoML_DSNA/edit/main/setup.py",
+    url="https://github.com/enisbaskapan/AutoML_DSNA",
     packages=setuptools.find_packages(),
     python_requires=">=3.10",)
